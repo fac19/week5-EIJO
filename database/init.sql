fbff8ab98a71403301ff460b2d7b75f0f6364ff7
Chloe <chloeh24@hotmail.com>
2020-04-02 19:10:12 +0100

    edit port number

--- database/init.sql
+++ database/init.sql
@@ -1,19 +1,22 @@
 BEGIN;
 
-DROP TABLE IF EXISTS recipes;
+    DROP TABLE IF EXISTS recipes;
 
-CREATE TABLE recipes(
-    id SERIAL PRIMARY KEY,
-    recipeTitle VARCHAR(255),
-    type VARCHAR(255),
-    ingredients VARCHAR(255),
-    method TEXT,
-    vegetarian BOOLEAN
-);
+    CREATE TABLE recipes
+    (
+        id SERIAL PRIMARY KEY,
+        recipeTitle VARCHAR(255),
+        type VARCHAR(255),
+        ingredients VARCHAR(255),
+        method TEXT,
+        vegetarian BOOLEAN
+    );
 
-INSERT INTO recipes(recipeTitle, type, ingredients, method, vegetarian) VALUES
-('Pesto pasta', 'dinner', 'pasta, pesto', 'cook it', '1'),
-('Shepherds pie', 'dinner', 'mash potato, meat', 'oven it', '0'),
-('pancakes', 'breakfast', 'eggs + stuff', 'flip it', '1');
+    INSERT INTO recipes
+        (recipeTitle, type, ingredients, method, vegetarian)
+    VALUES
+        ('Pesto pasta', 'dinner', 'pasta, pesto', 'cook it', '1'),
+        ('Shepherds pie', 'dinner', 'mash potato, meat', 'oven it', '0'),
+        ('pancakes', 'breakfast', 'eggs + stuff', 'flip it', '1');
 
-COMMIT;
+    COMMIT;
